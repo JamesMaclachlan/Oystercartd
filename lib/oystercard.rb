f474fcf72096d3fd48d241d4ee4a9569b9fe8749
James Maclachlan <james.maclachlan@meetupcall.com>
2020-04-16 15:14:29 +0100

    irb works but tests aren't green

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -24,6 +24,7 @@ class Oystercard
   end
 
   def touch_in
+    fail "Insufficient balance to touch in" if balance < 1
     @in_journey = true
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,18 +6,28 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
-  describe '#top_up' do
-    it { is_expected.to respond_to(:top_up).with(1).argument }
-
-    it 'can top up the balance' do
-      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
-    end
+  before do
+    maximum_balance = Oystercard::MAXIMUM_BALANCE
   end
 
-  it 'raises an error if the maximum balance is exceeded' do
-    maximum_balance = Oystercard::MAXIMUM_BALANCE
-    subject.top_up(maximum_balance)
-    expect{ subject.top_up 1 }.to raise_error 'Maximum balance of #{maximum_balance} exceeded'
+  it 'will not touch in if below minimum balance' do
+    expect{ subject.touch_in }.to raise_error "Insufficient balance to touch in"
+  end
+
+  context "all top_up instances" do
+    describe '#top_up' do
+      it { is_expected.to respond_to(:top_up).with(1).argument }
+
+      it 'can top up the balance' do
+        expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+      end
+    end
+
+    it 'raises an error if the maximum balance is exceeded' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      expect{ subject.top_up 1 }.to raise_error 'Maximum balance of #{maximum_balance} exceeded'
+    end
   end
 
   describe '#deduct' do
@@ -35,6 +45,12 @@ describe Oystercard do
   it 'can touch in' do
     subject.touch_in
     expect(subject).to be_in_journey
+  end
+
+  it 'can touch out' do
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_journey
   end
 
 end
